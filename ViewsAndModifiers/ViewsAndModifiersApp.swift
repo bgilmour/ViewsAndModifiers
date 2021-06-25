bb23b6b36d194062a24c3dbffcb5572554e6f45c
Bruce Gilmour <bruce.gilmour@me.com>
2021-06-25 14:28:52 +0100

    Initial Commit

---
+++ ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Bruce Gilmour on 2021-06-25.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
