b0a0e7bb29d36a049a077cb37bdd3b1fa30d035c
Bruce Gilmour <bruce.gilmour@me.com>
2021-06-27 02:01:16 +0100

    Completed solution for ViewsAndModifiers
    This change contains my completed solution for project 3 from the 100 Days Of SwiftUI tutorial created by Paul Hudson (@twostraws). It includes my interpretation of the custom modifier challenge step. The other challenge steps are implemented as additional updates to project 1 and project 2.

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -8,9 +8,93 @@
 import SwiftUI
 
 struct ContentView: View {
+
     var body: some View {
-        Text("Hello, world!")
+        GridStack(rows: 4, columns: 4) { row, col in
+            VStack {
+                Text("R\(row)")
+                Image(systemName: "\(row * 4 + col).circle")
+                Text("C\(col)")
+            }
+        }
+        .bigBlueStyle()
+    }
+}
+
+struct GridStack<Content: View>: View {
+    let rows: Int
+    let columns: Int
+    let content: (Int, Int) -> Content
+
+    init(rows: Int, columns: Int, @ViewBuilder content: @escaping (Int, Int) -> Content) {
+        self.rows = rows
+        self.columns = columns
+        self.content = content
+    }
+
+    var body: some View {
+        VStack {
+            ForEach(0 ..< rows, id: \.self) { row in
+                HStack {
+                    ForEach(0 ..< columns, id: \.self) { column in
+                        self.content(row, column)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.black)
             .padding()
+            .background(Color.yellow)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
+}
+
+extension View {
+    func titleStyle() -> some View {
+        self.modifier(Title())
+    }
+}
+
+struct Watermark: ViewModifier {
+    var text: String
+
+    func body(content: Content) -> some View {
+        ZStack(alignment: .bottomTrailing) {
+            content
+            Text(text)
+                .font(.caption)
+                .foregroundColor(.white)
+                .padding(5)
+                .background(Color.black)
+        }
+    }
+}
+
+extension View {
+    func watermarked(with text: String) -> some View {
+        self.modifier(Watermark(text: text))
+    }
+}
+
+struct BigBlue: ViewModifier {
+
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.blue)
+    }
+}
+
+extension View {
+    func bigBlueStyle() -> some View {
+        self.modifier(BigBlue())
     }
 }
 
